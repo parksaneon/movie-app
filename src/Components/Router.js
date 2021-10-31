9e977a2026851d02e0cbbef1d382378769650172
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-10-31 21:43:12 +0900

    apply Switch

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
 
 const Routes = () => (
   <Router>
-    <>
+    <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" component={TV} />
       <Route path="/search" component={Search} />
       <Redirect from="*" to="/" />
-    </>
+    </Switch>
   </Router>
 );
 
