36d17357a89335740749399a5500b051eff3ed75
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-10-31 22:33:51 +0900

    apply withRouter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Routes from "../Components/Router";
+import GlobalStyles from "./GlobalStyles";
 
 function App() {
   return (
     <>
       <Routes />
+      <GlobalStyles />
     </>
   );
 }
