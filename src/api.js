3c5b60333a5c7260071ff9d092bdb08f42f90aca
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-11-01 20:34:30 +0900

    feat : add two API object

--- src/api.js
+++ src/api.js
@@ -8,4 +8,14 @@ const api = axios.create({
   },
 });
 
-export default api;
+export const tvApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+};
+
+export const moviesApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+};
