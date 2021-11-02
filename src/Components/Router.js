537b08bba5a4a5d66deab219b39a2ebb7ea27abf
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-11-02 20:45:39 +0900

    feat : DetailComponente setState

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -8,6 +8,7 @@ import {
 import Home from "../Routes/Home";
 import TV from "../Routes/TV";
 import Search from "../Routes/Search";
+import Detail from "../Routes/Detail";
 import Header from "./Header";
 
 const Routes = () => (
@@ -18,6 +19,8 @@ const Routes = () => (
         <Route path="/" exact component={Home} />
         <Route path="/tv" component={TV} />
         <Route path="/search" component={Search} />
+        <Route path="/movie/:id" component={Detail} />
+        <Route path="/show/:id" component={Detail} />
         <Redirect from="*" to="/" />
       </Switch>
     </>
