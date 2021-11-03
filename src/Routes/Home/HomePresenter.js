8e69ab9e5b2b82cdc503916bf4a43fee420a3604
parksaneon <47975991+parksaneon@users.noreply.github.com>
2021-11-03 20:30:46 +0900

    feat : Presenter render

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -2,27 +2,36 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import Section from "../../Components/Section";
+import Loader from "../../Components/Loader";
 
 const Container = styled.div`
   padding: 0px 10px;
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  loading ? null : (
+  loading ? (
+    <Loader />
+  ) : (
     <Container>
       {nowPlaying && nowPlaying.length > 0 && (
         <Section title="Now Playing">
-          {nowPlaying.map((movie) => movie.title)}
+          {nowPlaying.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
       {upcoming && upcoming.length > 0 && (
-        <Section title="Now Playing">
-          {nowPlaying.map((movie) => movie.title)}
+        <Section title="Upcoming Movies">
+          {upcoming.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
       {popular && popular.length > 0 && (
         <Section title="Popular Movies">
-          {popular.map((movie) => movie.title)}
+          {popular.map((movie) => (
+            <span key={movie.id}>{movie.title}</span>
+          ))}
         </Section>
       )}
     </Container>
@@ -31,7 +40,7 @@ const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
 HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
   popular: PropTypes.array,
-  upcomfing: PropTypes.array,
+  upcoming: PropTypes.array,
   loading: PropTypes.bool.isRequired,
   error: PropTypes.string,
 };
